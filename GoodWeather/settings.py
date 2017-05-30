c44b9dd0eb626259641c0add1c4786a35ad47c4e
ASSASSIN-NIGHTFURY <assassin-nightfury-1027@outlook.com>
2017-05-30 09:21:00 +0800

    Update settings.py

--- GoodWeather/settings.py
+++ GoodWeather/settings.py
@@ -71,7 +71,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.normpath(os.path.join(PROJECT_DIR, 'GoodWeather/static/'))
+STATIC_ROOT = os.path.normpath(os.path.join(PROJECT_PATH, 'GoodWeather/static/'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
