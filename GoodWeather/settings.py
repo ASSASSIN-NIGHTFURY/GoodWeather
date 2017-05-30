afd23c96396a4cf1178d9dbb687722d23ca3df9c
assassin94mean <kevintaylor19901010@gmail.com>
2017-05-30 09:09:58 +0800

    second commit for deployment

--- GoodWeather/settings.py
+++ GoodWeather/settings.py
@@ -7,7 +7,7 @@ from django.conf import global_settings
 
 # Load environment variables from a .env file so that they can be read later in settings via os.environ.get().
 # See https://github.com/theskumar/python-dotenv.
-PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
+PROJECT_PATH = os.path.dirname(os.path.absname(__file__))
 #dotenv.load_dotenv(os.path.join(PROJECT_PATH, ".env"))
 
 DEBUG = True
@@ -71,7 +71,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.normpath(os.path.join(PROJECT_DIR, 'GoodWeather/static/'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
